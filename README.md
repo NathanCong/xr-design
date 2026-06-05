882003cc8196b8771235a71a0fd526c8f0284bbb
NathanCong <congyunan@gmail.com>
2026-06-06 06:46:13 +0800

    feat: 项目初始化

--- README.md
+++ README.md
@@ -10,31 +10,37 @@ pnpm serve
 
 ## 项目构建
 
-### 1. 构建组件库（CommonJS）
+### 构建组件库（CommonJS 版本）
 
 ```bash
 pnpm build:cjs
 ```
 
-### 2. 构建组件库（ES Module）
+### 构建组件库（ES Module 版本）
 
 ```bash
 pnpm build:esm
 ```
 
-### 3. 构建组件库（UMD）
+### 构建组件库（UMD 版本）
 
 ```bash
 pnpm build:umd
 ```
 
-### 4. 构建组件库（CommonJS + ES Module + UMD）
+### 构建组件库类型（Types）
+
+```bash
+pnpm build:types
+```
+
+### 构建组件库（CommonJS + ES Module + UMD + Types）
 
 ```bash
 pnpm build:dist
 ```
 
-### 5. 构建预览页
+### 构建预览页
 
 ```bash
 pnpm build:site
@@ -42,13 +48,13 @@ pnpm build:site
 
 ## 代码检测
 
-### 1. 代码检查（不修复）
+### 代码检查（不修复）
 
 ```bash
 pnpm check
 ```
 
-### 2. 代码检查（带修复）
+### 代码检查（带修复）
 
 ```bash
 pnpm check:fix
--- vite.config.cjs
+++ vite.config.cjs
@@ -3,8 +3,11 @@ const { svelte } = require('@sveltejs/vite-plugin-svelte')
 const { resolve } = require('path')
 
 const SRC_DIR = resolve(__dirname, 'src')
-const DIST_DIR = resolve(__dirname, 'dist')
 const SITE_DIR = resolve(__dirname, 'site')
+const DIST_CJS_DIR = resolve(__dirname, 'dist/cjs')
+const DIST_ESM_DIR = resolve(__dirname, 'dist/esm')
+const DIST_UMD_DIR = resolve(__dirname, 'dist/umd')
+const DIST_TYPES_DIR = resolve(__dirname, 'dist/types')
 
 const commonPlugins = [
   svelte({
@@ -30,6 +33,20 @@ module.exports = defineConfig(({ command, mode }) => {
         host: 'localhost',
         port: 5173,
         open: true,
+      },
+    }
+  }
+  /**
+   * 构建环境（site）
+   */
+  if (command === 'build' && mode === 'site') {
+    return {
+      plugins: commonPlugins,
+      resolve: commonResolve,
+      build: {
+        outDir: SITE_DIR,
+        emptyOutDir: true,
+        sourcemap: false,
       },
     }
   }
